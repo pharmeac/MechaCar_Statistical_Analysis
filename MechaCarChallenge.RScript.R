78869a8f5e625bd870b66e5bf001d97a4354cc9b
pharmeac <emily@Emilys-MacBook-Pro.local>
2022-12-08 23:33:02 -0500

    deliverable 3 code added

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -15,3 +15,25 @@ total_summary <- susp_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Vari
 # Create a lot_summary table
 lot_summary <- susp_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep') 
 
+# determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500
+t.test(susp_table$PSI,mu=1500)
+
+#Lot1 data
+Lot_1 <- subset(susp_table, Manufacturing_Lot=='Lot1', select=(PSI))
+
+#t.test on Lot 1
+t.test(Lot_1$PSI,mu=1500)
+
+#Lot2 data
+Lot_2 <- subset(susp_table, Manufacturing_Lot =="Lot2", select=(PSI))
+
+#t.test on Lot 2
+t.test(Lot_2$PSI,mu=1500)
+
+#Lot3 data
+Lot_3 <- subset(susp_table, Manufacturing_Lot=='Lot3', select=(PSI))
+
+#t.test on Lot3
+t.test(Lot_3$PSI,mu=1500)
+
+
