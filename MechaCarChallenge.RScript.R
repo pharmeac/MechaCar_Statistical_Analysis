2ad632acc69bb1e88527f6e33a1250f7caa36226
pharmeac <emily@Emilys-MacBook-Pro.local>
2022-12-13 20:14:37 -0500

    updated r script for deliverable 1

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,8 +3,8 @@ library(tidyverse)
 # Deliverable 1
 MechCar_mpg_df <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import dataset
 
-lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechCar_mpg_df) #generate multiple linear regression model
-summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechCar_mpg_df))#generate summary statistics
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechCar_mpg_df) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechCar_mpg_df))#generate summary statistics
 
 # Deliverable2
 susp_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F) #import dataset
